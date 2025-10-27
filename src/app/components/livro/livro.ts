c53090bd9e0cfd25c79a43f016f4baabfed83959
FVS <feliven@gmail.com>
2025-10-27 13:49:25 -0300

    implementados pipes e criado pipe para autoria

--- src/app/components/livro/livro.ts
+++ src/app/components/livro/livro.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 
 import { ModalLivro } from '../../views/modal-livro/modal-livro';
 import { InterfaceLivro } from '../../models/interfaces';
+import { AutoriaPipe } from '../../pipes/autoria-pipe';
 
 @Component({
   selector: 'app-livro',
   templateUrl: './livro.html',
   styleUrls: ['./livro.css'],
-  imports: [ModalLivro, CommonModule],
+  imports: [ModalLivro, CommonModule, AutoriaPipe],
 })
 export class Livro {
   @Input() livro: InterfaceLivro;
