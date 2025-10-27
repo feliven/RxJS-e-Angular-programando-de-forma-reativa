bf0da96b355a1e0aa1fc860bc2a519b128fa0ed1
FVS <feliven@gmail.com>
2025-10-27 12:35:26 -0300

    converterResultadoParaInterfaceLivro

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -1,4 +1,4 @@
-export interface Livro {
+export interface InterfaceLivro {
   title: string;
   authors?: string[];
   publisher?: string;
--- src/app/views/lista-livros/lista-livros.ts
+++ src/app/views/lista-livros/lista-livros.ts
@@ -4,18 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 import { Livro } from '../../components/livro/livro';
+import { InterfaceLivro } from '../../models/interfaces';
 import { LivroService } from '../../service/livro-service';
+import { GoogleBookVolume } from '../../models/interfaces';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.html',
   styleUrls: ['./lista-livros.css'],
-  imports: [Livro, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, Livro],
 })
 export class ListaLivros implements OnDestroy {
-  listaLivros: any[] = [];
+  listaLivros: InterfaceLivro[] = [];
   campoBusca: string = '';
   assinatura: Subscription;
+  livro: InterfaceLivro;
 
   constructor(private livroService: LivroService) {}
 
@@ -24,6 +27,20 @@ export class ListaLivros implements OnDestroy {
       // next: (retornoDaAPI) => console.log(retornoDaAPI),
       error: (erro) => console.log(erro),
       complete: () => console.log('Observable completo'),
+    });
+  }
+
+  converterResultadoParaInterfaceLivro(googleBookVolumes: GoogleBookVolume[]) {
+    googleBookVolumes.forEach((volume) => {
+      this.livro = {
+        title: volume.volumeInfo?.title,
+        authors: volume.volumeInfo?.authors,
+        publisher: volume.volumeInfo?.publisher,
+        publishedDate: new Date(volume.volumeInfo?.publishedDate),
+        description: volume.volumeInfo?.description,
+        previewLink: volume.volumeInfo?.previewLink,
+        thumbnail: volume.volumeInfo?.imageLinks.thumbnail,
+      };
     });
   }
 
